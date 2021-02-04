72c55217abe33139e771e1b426f5a9212fb3e16e
nbradac <nate.bradac@opendatagroup.com>
2021-02-04 13:02:55 -0600

    initial commit

---
+++ elmodelerino.py
@@ -0,0 +1,19 @@
+#fastscore.slot.0: in-use
+#fastscore.slot.1: in-use
+
+#modelop.score
+def action(datum):
+    print(datum, flush=True)
+    i = datum['i']
+    out = {
+            'integer' : 123 * i,
+            'float' : 123.456 + i,
+            'boolean': i % 2 == 0,
+            'string': 'thequickbrownfoxjumpedoverthelazydog'[0:i]
+            }
+    yield out
+
+#x = action({'i': 5})
+#print(next(x))
+#x = action({'i': 12})
+#print(next(x))
